3ba287b8107e83b3af1b678e513ec6a37466e5b2
Cody Edgington <cody.edgington@gmail.com>
2025-07-04 13:44:53 -0700

    Change timer

--- init.py
+++ init.py
@@ -16,4 +16,4 @@ while True:
     led.on()  # Turn the LED on
     sleep(1)  # Wait for 1 second
     led.off() # Turn the LED off
-    sleep(1)  # Wait for 1 second
+    sleep(0.2)  # Wait for 1 second
