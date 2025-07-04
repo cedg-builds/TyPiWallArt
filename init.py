ddbc8dd204daa96f11ae07cf366e054cdca837f6
Cody Edgington <cody.edgington@gmail.com>
2025-07-04 12:58:57 -0700

    Boot script

---
+++ init.py
@@ -0,0 +1,19 @@
+
+
+#This script is called into from the boot script
+
+
+print("We booted yall")
+
+from gpiozero import LED
+from time import sleep
+
+# Define the GPIO pin connected to the LED
+# Replace 17 with the actual GPIO pin number you are using
+led = LED(3) 
+
+while True:
+    led.on()  # Turn the LED on
+    sleep(1)  # Wait for 1 second
+    led.off() # Turn the LED off
+    sleep(1)  # Wait for 1 second
